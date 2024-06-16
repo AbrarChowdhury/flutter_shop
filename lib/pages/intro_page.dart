636b76e15e362b766879803b3c6a7d9b56d14000
AbrarChowdhury <abrar.chowdhury99@gmail.com>
2024-06-17 05:33:46 +0600

    get the images working

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -6,7 +6,16 @@ class IntroPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
+      backgroundColor: Colors.grey[300],
+      body: 
+        Center(
+          child: Column(
+            children: [
+              Image(image:AssetImage('images/logo.png')),
+              Text("save money, don't just do it"),
+            ],
+          ),
+        ),
     );
   }
 }
