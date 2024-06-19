d1d0a303a2afccd4e2905873564283e8b028e894
AbrarChowdhury <abrar.chowdhury99@gmail.com>
2024-06-19 09:13:49 +0600

    creaete drawer

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_e_commerse/components/bottom_nav_bar.dart';
 import 'package:flutter_e_commerse/pages/cart_page.dart';
@@ -26,7 +27,57 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       backgroundColor: Colors.grey[300],
+      appBar: AppBar(
+        leading: Builder(
+          builder: (context) =>IconButton(
+              icon: Icon(Icons.menu), 
+              onPressed: () {  
+                Scaffold.of(context).openDrawer();
+              },
+            ),
+          
+        ),
+      ),
+      drawer: Drawer(
+        backgroundColor: Colors.grey[900],
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            
+
+            Column(
+              children: [
+                DrawerHeader(child: Image.asset('images/logo.png', color:Colors.white)),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0, ),
+                  child: ListTile(
+                    leading: Icon(Icons.home),
+                    title: Text("Home", style:TextStyle(color: Colors.white),),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 25.0,),
+                  child: ListTile(
+                    leading: Icon(Icons.info),
+                    title: Text("About", style:TextStyle(color: Colors.white),),
+                  ),
+                ),
+              ],
+            ),
+            
+
+            Padding(
+              padding: const EdgeInsets.only(left: 25.0, bottom: 25),
+              child: ListTile(
+                leading: Icon(Icons.logout),
+                title: Text("Logout", style:TextStyle(color: Colors.white),),
+              ),
+            )
+          ],
+        )
+      ),
       body: _pages[_selectedIndex],
       bottomNavigationBar: BottomNavBar(onTabChange: (index)=>handleTabChange(index),),
     );
