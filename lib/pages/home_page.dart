c89bb706d20f9f6c30716046987af40c24ef7fcb
AbrarChowdhury <abrar.chowdhury99@gmail.com>
2024-06-19 07:53:46 +0600

    make bottom nav functional

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,14 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_e_commerse/bottom_nav_bar.dart';
+import 'package:flutter_e_commerse/components/bottom_nav_bar.dart';
+import 'package:flutter_e_commerse/pages/cart_page.dart';
+import 'package:flutter_e_commerse/pages/shop_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  void handleTabChange(int index){
+    setState(() {
+      _selectedIndex=index;
+      print(_selectedIndex);
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage()
+  ];
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[100],
-      bottomNavigationBar: BottomNavBar(),
+      backgroundColor: Colors.grey[300],
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavBar(onTabChange: (index)=>handleTabChange(index),),
     );
   }
 }
